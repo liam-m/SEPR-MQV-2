5221729d99fcef390c46e9a5788aa9c8920eacf0
Liam Mullane <lm987@york.ac.uk>
2014-02-19 22:33:50 +0000

    Refactor ButtonText

--- BTC/src/lib/ButtonText.java
+++ BTC/src/lib/ButtonText.java
@@ -11,9 +11,9 @@ public class ButtonText {
 
 	private int x, y, width, height, ox, oy;
 	private String text;
-	private org.newdawn.slick.Color colourDefault, colourHover, colourUnavailable;
+	private org.newdawn.slick.Color default_colour, hover_colour, unavailable_colour;
 	private Action action;
-	private boolean available;
+	private boolean is_available;
 	
 	//#What is ox/oy
 	public ButtonText(String text, Action action, int x, int y, int w, int h, int ox, int oy) {
@@ -23,12 +23,12 @@ public class ButtonText {
 		this.y = y;
 		width = w;
 		height = h;
-		this.ox = ox;
-		this.oy = oy;
-		colourDefault = new org.newdawn.slick.Color(0, 128, 0);
-		colourHover = new org.newdawn.slick.Color(128, 128, 128);
-		colourUnavailable = new org.newdawn.slick.Color(64, 64, 64);
-		available = true;
+		this.ox = ox; // x position to print text
+		this.oy = oy; // y position to print text
+		default_colour = new org.newdawn.slick.Color(0, 128, 0);
+		hover_colour = new org.newdawn.slick.Color(128, 128, 128);
+		unavailable_colour = new org.newdawn.slick.Color(64, 64, 64);
+		is_available = true;
 	}
 	
 	public ButtonText(String text, Action action, int x, int y, int w, int h) {
@@ -40,10 +40,10 @@ public class ButtonText {
 		height = h;
 		this.ox = (w - (text.length() * 8)) / 2;
 		this.oy = (h - 8) / 2;
-		colourDefault = new org.newdawn.slick.Color(0, 128, 0);
-		colourHover = new org.newdawn.slick.Color(128, 128, 128);
-		colourUnavailable = new org.newdawn.slick.Color(64, 64, 64);
-		available = true;
+		default_colour = new org.newdawn.slick.Color(0, 128, 0);
+		hover_colour = new org.newdawn.slick.Color(128, 128, 128);
+		unavailable_colour = new org.newdawn.slick.Color(64, 64, 64);
+		is_available = true;
 	}
 	
 	public boolean isMouseOver(int mx, int my) {
@@ -53,40 +53,32 @@ public class ButtonText {
 	public boolean isMouseOver() { 
 		return isMouseOver(input.mouseX(), input.mouseY()); 
 	}
-	/**
-	 * Sets the string of text used
-	 * @param newText - The string to be used
-	 */
-	//#Needed?
+
 	public void setText(String newText) {
 		text = newText;
 	}
 	
 	/**
 	 * Sets the button text to available - Changing the color to the one specified in ButtonText()
-	 * @param available - value of the availability, either True or False
+	 * @param is_available - value of the availability, either True or False
 	 */
-	public void setAvailability(boolean available) {
-		this.available = available;
+	public void setAvailability(boolean is_available) {
+		this.is_available = is_available;
 	}
 	
-	//#Da Faq?
 	public void act() {
-		if (!available) return;
-		action.action();
+		if (is_available)
+			action.action();
 	}
-	/**
-	 * Draws the button text which reacts to mouse interactions 
-	 */
-	//#Needed?
+	
 	public void draw() {
-		if (!available) {
-			graphics.setColour(colourUnavailable);
+		if (!is_available) {
+			graphics.setColour(unavailable_colour);
 		}
 		else if (isMouseOver()) {
-			graphics.setColour(colourHover);
+			graphics.setColour(hover_colour);
 		} else {
-			graphics.setColour(colourDefault);
+			graphics.setColour(default_colour);
 		}
 		graphics.print(text, x + ox, y + oy);
 	}
