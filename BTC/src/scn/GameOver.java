11b05507e4e1e3eee9096f6c206b6922a89cf908
Liam Mullane <lm987@york.ac.uk>
2014-02-19 23:42:49 +0000

    Refactor GameOver

--- BTC/src/scn/GameOver.java
+++ BTC/src/scn/GameOver.java
@@ -16,28 +16,28 @@ public class GameOver extends Scene {
 	/**
 	 * Text box to write the details of the game failure
 	 */
-	private lib.TextBox textBox;
+	private lib.TextBox text_box;
 	
 	/**
 	 * Used to position the explosion, and provide graphical feedback of how and where the player failed
 	 */
-	private Aircraft crashedPlane1;
-	private Aircraft crashedPlane2;
+	private Aircraft crashed_plane_1;
+	private Aircraft crashed_plane_2;
 	/**
 	 * A random number of deaths caused by the crash
 	 */
-	private int deaths;
+	private int num_deaths;
 	
-	private int score; //passed in when game is over;
+	private int score; // Passed in from Demo
 	
 	/**
 	 * The position of the crash - the vector midpoint of the positions of the two crashed planes
 	 */
-	private Vector crash;
+	private Vector crash_position;
 	/**
 	 * A sprite animation to handle the frame by frame drawing of the explosion
 	 */
-	private SpriteAnimation explosionAnim;
+	private SpriteAnimation explosion_animation;
 	/**
 	 * The explosion image to use for the animation
 	 */
@@ -53,76 +53,75 @@ public class GameOver extends Scene {
 	/**
 	 * Constructor for the Game Over scene
 	 * @param main main containing the scene
-	 * @param plane1 the first plane involved in the crash
-	 * @param plane2 the second plane involved in the crash
+	 * @param plane_1 the first plane involved in the crash
+	 * @param plane_2 the second plane involved in the crash
+	 * @param score the score when the game finished
 	 */
-	public GameOver(Main main, Aircraft plane1, Aircraft plane2, int score) {
+	public GameOver(Main main, Aircraft plane_1, Aircraft plane_2, int score) {
 		super(main);
-		crashedPlane1 = plane1;
-		crashedPlane2 = plane2;
-		crash = new Vector(plane1.getPosition().getX(), plane1.getPosition().getY(), 0);
-		int framesAcross = 8;
-		int framesDown = 4;
+		crashed_plane_1 = plane_1;
+		crashed_plane_2 = plane_2;
+		crash_position = new Vector(plane_1.getPosition().getX(), plane_1.getPosition().getY(), 0);
+		int frames_across = 8;
+		int frames_down = 4;
 		this.score = score;
 		explosion = graphics.newImage("gfx" + File.separator + "explosionFrames.png");
-		Vector midPoint = crashedPlane1.getPosition().add(crashedPlane2.getPosition()).scaleBy(0.5);
-		Vector explosionPos = midPoint.sub( new Vector(explosion.width()/(framesAcross*2), explosion.height()/(framesDown*2), 0) );
-		explosionAnim = new SpriteAnimation(explosion, (int)explosionPos.getX(), (int)explosionPos.getY(), 6, 16, framesAcross, framesDown, false);
+		Vector midpoint = crashed_plane_1.getPosition().add(crashed_plane_2.getPosition()).scaleBy(0.5);
+		Vector explosion_position = midpoint.sub(new Vector(explosion.width()/(frames_across*2), explosion.height()/(frames_down*2), 0));
+		explosion_animation = new SpriteAnimation(explosion, (int)explosion_position.getX(), (int)explosion_position.getY(), 6, 16, frames_across, frames_down, false);
 	}
 	
 	/**
-	 * initialises the random number of deaths, timer, and text box with strings to be written about the game failure
+	 * Initialises the random number of deaths, timer, and text box with strings to be written about the game failure
 	 */
 	@Override
 	public void start() {
 		playSound(audio.newSoundEffect("sfx" + File.separator + "crash.ogg"));
-		deaths = (int)( Math.random() * 500) + 300;
+		num_deaths = (int)(Math.random() * 500) + 300;
 		timer = 0;
-		textBox = new lib.TextBox(64, 186, window.width() - 128, window.height() - 96, 32);
-		textBox.addText(String.valueOf(deaths) + " people died in the crash.");
-		textBox.delay(0.4);
-		textBox.addText("British Bearways is facing heavy legal pressure from the family and loved-ones of the dead and an investigation is underway.");
-		textBox.newline();
-		textBox.delay(0.8);
-		textBox.addText("The inquiry into your incompetence will lead to humanity discovering your true nature.");
-		textBox.newline();
-		textBox.delay(0.8);
-		textBox.addText("The guilt for the death you have caused and your failure to pass as a human will gnaw at you and you will revert to drinking in an attempt to cope.");
-		textBox.newline();
-		textBox.newline();
-		textBox.delay(0.8);
-		textBox.addText("With no income, there will be no way your family can survive the fast-approaching winter months.");
-		textBox.newline();
-		textBox.newline();
-		textBox.delay(0.8);
-		textBox.addText("Game Over.");
+		text_box = new lib.TextBox(64, 186, window.width() - 128, window.height() - 96, 32);
+		text_box.addText(String.valueOf(num_deaths) + " people died in the crash.");
+		text_box.delay(0.4);
+		text_box.addText("British Bearways is facing heavy legal pressure from the family and loved-ones of the dead and an investigation is underway.");
+		text_box.newline();
+		text_box.delay(0.8);
+		text_box.addText("The inquiry into your incompetence will lead to humanity discovering your true nature.");
+		text_box.newline();
+		text_box.delay(0.8);
+		text_box.addText("The guilt for the death you have caused and your failure to pass as a human will gnaw at you and you will revert to drinking in an attempt to cope.");
+		text_box.newlines(2);
+		text_box.delay(0.8);
+		text_box.addText("With no income, there will be no way your family can survive the fast-approaching winter months.");
+		text_box.newlines(2);
+		text_box.delay(0.8);
+		text_box.addText("Game Over.");
 	}
 
-	@Override
 	/**
 	 * If it runs before the explosion has finished, update the explosion
 	 * otherwise, update text box instead
 	 */
+	@Override
 	public void update(double time_difference) {
-		if (explosionAnim.hasFinished()){
+		if (explosion_animation.hasFinished()){
 			timer += time_difference;
-			textBox.update(time_difference);
+			text_box.update(time_difference);
 		} else {
-			explosionAnim.update(time_difference);
+			explosion_animation.update(time_difference);
 		}
 	}
 
 	@Override
-	public void mousePressed(int key, int x, int y) {}
+	public void mousePressed(int key, int x, int y) { }
 
 	@Override
-	public void mouseReleased(int key, int x, int y) {}
+	public void mouseReleased(int key, int x, int y) { }
 
-	@Override
 	/**
 	 * Tracks if any keys are pressed when the game over screen begins
 	 * Prevents the scene instantly ending due to a key press from previous scene
 	 */
+	@Override
 	public void keyPressed(int key) {
 		keyPressed = key;
 	}
@@ -133,31 +132,31 @@ public class GameOver extends Scene {
 	@Override
 	public void keyReleased(int key) {
 		if (key == keyPressed) {
-			main.closeScene();
-			main.closeScene();
+			main.closeScene(); // Close this screen
+			main.closeScene(); // Close difficulty select
 		}
 	}
 
-	@Override
 	/**
 	 * Draws game over
 	 * If explosion has finished, draw the textbox
 	 * Otherwise, draw the planes and explosion
 	 */
+	@Override
 	public void draw() {
 		graphics.setColour(graphics.green);
-		graphics.printCentred(crashedPlane1.getName() + " crashed into " + crashedPlane2.getName() + ".", 0, 32, 2, window.width());
+		graphics.printCentred(crashed_plane_1.getName() + " crashed into " + crashed_plane_2.getName() + ".", 0, 32, 2, window.width());
 		graphics.printCentred("Total score: " + String.valueOf(score), 0, 64, 4, window.width());
-		if (explosionAnim.hasFinished()) {
-			textBox.draw();
+		if (explosion_animation.hasFinished()) {
+			text_box.draw();
 		} else {
-			crashedPlane1.draw((int) crashedPlane1.getPosition().getZ());
-			crashedPlane2.draw((int) crashedPlane1.getPosition().getZ());
-			Vector midPoint = crash.add(crashedPlane2.getPosition()).scaleBy(0.5);
+			crashed_plane_1.draw((int) crashed_plane_1.getPosition().getZ());
+			crashed_plane_2.draw((int) crashed_plane_1.getPosition().getZ());
+			Vector midPoint = crash_position.add(crashed_plane_2.getPosition()).scaleBy(0.5);
 			double radius = 20; // Radius of explosion
 			graphics.setColour(graphics.red);
 			graphics.circle(false, midPoint.getX(), midPoint.getY(), radius);
-			explosionAnim.draw();
+			explosion_animation.draw();
 		}
 		int opacity = (int)(255 * Math.sin(timer));
 		graphics.setColour(0, 128, 0, opacity);
@@ -165,7 +164,7 @@ public class GameOver extends Scene {
 	}
 
 	@Override
-	public void close() {}
+	public void close() { }
 
 	@Override
 	public void playSound(Sound sound) {
